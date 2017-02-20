e2ae8612eb1c2b88eed4d0c99cd1d7ed206c77ef
root <root@ip-172-31-43-113.eu-west-1.compute.internal>
2017-02-20 15:23:38 +0000

    Deathstroke Build

--- Resources/entrypoint.sh
+++ Resources/entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 sed -i -e '/SELENIUM_HUB_URL/d' /opt/fitnesse/plugins.properties
-
-if [ "SELENIUM_PROVISION" = "true" ]
-then
-   echo "SELENIUM_HUB_URL = http://selenium-hub:4444/wd/hub/" >> /opt/fitnesse/plugins.properties
-else
-   echo "SELENIUM_HUB_URL = ${SELENIUM_HUB_URL}" >> /opt/fitnesse/plugins.properties
-fi
+echo "SELENIUM_HUB_URL = ${SELENIUM_HUB_URL}" >> /opt/fitnesse/plugins.properties
 
 java -jar fitnesse-standalone.jar -a passwords.txt
 
--- Standalone/vars.sh
+++ Standalone/vars.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #false if you already have an existing grid i.e you already have adop
 export SELENIUM_PROVISION="true"
-#export SELENIUM_HUB_URL=http://selenium-hub:4444/grid/console
+
+#Edit only when you have your own hub
+export SELENIUM_HUB_URL=http://selenium-hub:4444/wd/hub/
 
 
 #name of network. Any name if standalone if thru adop it should be the same as the network of your adop tools
